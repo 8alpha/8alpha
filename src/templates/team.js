f8656078f88fec41fbc520141289c438eddb5671
Alex Kelley <adkelley@users.noreply.github.com>
2019-04-23 09:27:35 -0700

    add: about-us

--- src/templates/team.js
+++ src/templates/team.js
@@ -1,7 +1,9 @@
 import React from "react";
 import styled from "styled-components";
-import { Link, graphql } from "gatsby";
+import { Link, StaticQuery, graphql } from "gatsby";
 import { Img } from "gatsby-image";
+
+import alexKelley from "../images/partners/alex-kelley.png";
 
 import Layout from "../components/layout";
 
@@ -39,9 +41,11 @@ const Photos = styled.div`
   flex-direction: row;
   justify-content: flex-start;
   flex-basis: 50%;
+  padding-top: 2rem;
 `;
 
-const Photo = styled.div``;
+const Photo = styled.div`
+`;
 
 const Bio = styled.div`
   color: white;
@@ -60,36 +64,85 @@ const Team = ({ data }) => {
   const bio = data.bio;
 
   return (
-    <Layout>
-      <Menu>
-        <MenuItem>
-          <StyledLink
-            to="/team/partners/nick-divehall/"
-            activeStyle={{ borderBottom: `2px solid white` }}
-          >
-            Partners
-          </StyledLink>
-        </MenuItem>
-        <MenuItem>
-          <StyledLink
-            to="/team/advisors/john-doe/"
-            activeStyle={{ borderBottom: `2px solid white` }}
-          >
-            Advisors
-          </StyledLink>
-        </MenuItem>
-      </Menu>
-      <Wrapper>
-        <Photos>
-          <div>
-            <Img fixed={data.alexKelley.childImageSharp.fixed} />
-          </div>
-        </Photos>
-        <Bio dangerouslySetInnerHTML={{ __html: bio.html }} />
-      </Wrapper>
-    </Layout>
+      <Layout>
+          <Menu>
+              <MenuItem>
+                  <StyledLink
+                      to="/team/about-us"
+                      activeStyle={{ borderBottom: `2px solid white` }}
+                  >
+                      About Us
+                  </StyledLink>
+              </MenuItem>
+              <MenuItem>
+                  <StyledLink
+                      to="/team/partners/nick-divehall/"
+                      activeStyle={{ borderBottom: `2px solid white` }}
+                  >
+                      Partners
+                  </StyledLink>
+              </MenuItem>
+              <MenuItem>
+                  <StyledLink
+                      to="/team/advisors/john-doe/"
+                      activeStyle={{ borderBottom: `2px solid white` }}
+                  >
+                      Advisors
+                  </StyledLink>
+              </MenuItem>
+          </Menu>
+          <Wrapper>
+              <Photos>
+                  <Photo>
+                      {/* <Img fixed={bioQuery.data.alexKelley.childImageSharp.fixed} /> */}
+                      <img src={alexKelley} alt="Alex Kelley" />
+                  </Photo>
+              </Photos>
+              <Bio dangerouslySetInnerHTML={{ __html: bio.html }} />
+          </Wrapper>
+      </Layout>
   );
 };
+
+/* const Team = () => {
+ *   return (
+ *     <StaticQuery
+ *       query={bioQuery}
+ *       render={data => {
+ *         return (
+ *           <Layout>
+ *             <Menu>
+ *               <MenuItem>
+ *                 <StyledLink
+ *                   to="/team/partners/nick-divehall/"
+ *                   activeStyle={{ borderBottom: `2px solid white` }}
+ *                 >
+ *                   Partners
+ *                 </StyledLink>
+ *               </MenuItem>
+ *               <MenuItem>
+ *                 <StyledLink
+ *                   to="/team/advisors/john-doe/"
+ *                   activeStyle={{ borderBottom: `2px solid white` }}
+ *                 >
+ *                   Advisors
+ *                 </StyledLink>
+ *               </MenuItem>
+ *             </Menu>
+ *             <Wrapper>
+ *               <Photos>
+ *                 <div>
+ *                   <Img fixed={data.alexKelley.childImageSharp.fixed} />
+ *                 </div>
+ *               </Photos>
+ *               <Bio dangerouslySetInnerHTML={{ __html: data.bio.html }} />
+ *             </Wrapper>
+ *           </Layout>
+ *         );
+ *       }}
+ *     />
+ *   );
+ * }; */
 
 export const bioQuery = graphql`
   query($slug: String!) {
@@ -106,16 +159,4 @@ export const bioQuery = graphql`
   }
 `;
 
-/* export const pic = graphql`
- * query picQuery {
- *     alexKelley: file(relativePath: { eq: "images/partners/alex-kelley.png" }) {
- *       childImageSharp {
- *         fixed(height: 150, width: 150) {
- *           ...GatsbyImageSharpFixed
- *         }
- *       }
- *     }
- *   }
- * `;
- *  */
 export default Team;
