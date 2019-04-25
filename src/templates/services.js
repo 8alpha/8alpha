fda1e62ec530a0594a3bb51ab82f30637c484062
Alex Kelley <adkelley@mac.com>
2019-04-24 19:59:21 -0700

    refact: menu

--- src/templates/services.js
+++ src/templates/services.js
@@ -1,29 +1,9 @@
 import React from "react";
+import PropTypes from "prop-types";
 import styled from "styled-components";
 import { Link, graphql } from "gatsby";
 
 import Layout from "../components/layout";
-
-const StyledLink = styled(Link)`
-  color: white;
-  text-decoration: none;
-  &:hover {
-    border-bottom: 2px solid white;
-  }
-`;
-
-const Menu = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  flex-direction: row;
-  justify-content: flex-start;
-`;
-
-const MenuItem = styled.div`
-  flex-basis: 10%;
-  font-weight: bold;
-  font-size: 2.5rem;
-`;
 
 const Section = styled.div`
   color: white;
@@ -42,39 +22,56 @@ const Section = styled.div`
   }
 `;
 
+const Menu = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: row;
+  justify-content: flex-start;
+`;
+
+const MenuItem = styled.div`
+  flex-basis: 12%;
+  font-weight: bold;
+  font-size: 2.5rem;
+`;
+
+const StyledLink = styled(Link)`
+  color: white;
+  text-decoration: none;
+  &:hover {
+    border-bottom: 2px solid white;
+  }
+`;
+
+const MenuFragment = ({ path, itemName }) => (
+  <MenuItem>
+    <StyledLink to={path} activeStyle={{ borderBottom: `2px solid white` }}>
+      <>{itemName}</>
+    </StyledLink>
+  </MenuItem>
+);
+
+MenuFragment.propTypes = {
+  path: PropTypes.string,
+  itemName: PropTypes.string,
+};
+
 const Services = ({ data }) => {
   const section = data.markdownRemark;
   return (
     <Layout>
       <Menu>
-        <MenuItem>
-          <StyledLink
-            to="/services/financial/"
-            activeStyle={{ borderBottom: `2px solid white` }}
-          >
-            Financial
-          </StyledLink>
-        </MenuItem>
-        <MenuItem>
-          <StyledLink
-            to="/services/marketing/"
-            activeStyle={{ borderBottom: `2px solid white` }}
-          >
-            Marketing
-          </StyledLink>
-        </MenuItem>
-        <MenuItem>
-          <StyledLink
-            to="/services/technical/"
-            activeStyle={{ borderBottom: `2px solid white` }}
-          >
-            Technical
-          </StyledLink>
-        </MenuItem>
+        <MenuFragment path="/services/financial" itemName="Financial" />
+        <MenuFragment path="/services/marketing/" itemName="Marketing" />
+        <MenuFragment path="/services/technical/" itemName="Technical" />
       </Menu>
       <Section dangerouslySetInnerHTML={{ __html: section.html }} />
     </Layout>
   );
+};
+
+Services.propTypes = {
+  data: PropTypes.object,
 };
 
 export const bio = graphql`
