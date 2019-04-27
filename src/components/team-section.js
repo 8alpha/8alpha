b9c5fecacd584881b82cedabb3c55342837b0091
Alex Kelley <adkelley@mac.com>
2019-04-26 22:07:06 -0700

    add: partner photos

--- src/components/team-section.js
+++ src/components/team-section.js
@@ -1,9 +1,12 @@
 import React from "react";
+import { graphql, StaticQuery } from "gatsby";
+import Img from "gatsby-image";
 import styled from "styled-components";
 
 const Container = styled.div`
   margin-top: 10rem;
   color: #dddddd;
+  margin-left: 10rem;
 `;
 
 const SectionHeader = styled.div`
@@ -12,10 +15,92 @@ const SectionHeader = styled.div`
   color: #dc143c;
 `;
 
+const Photo = styled.div`
+  flex: 0 0 25%;
+  margin-right: 2rem;
+  margin-top: 2rem;
+  /* border-style: solid;
+    * border-color: red; */
+`;
+
+const Photos = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: row;
+  justify-content: flex-start;
+  width: 100%;
+  /* border-style: solid;
+    * border-color: red; */
+`;
+
 const TeamSection = () => (
-  <Container>
-    <SectionHeader>Meet Our Team</SectionHeader>
-  </Container>
+  <StaticQuery
+    query={graphql`
+      fragment fluidImage on File {
+        childImageSharp {
+          fluid(maxWidth: 480, maxHeight: 720) {
+            ...GatsbyImageSharpFluid_withWebp
+          }
+        }
+      }
+      query {
+        alexKelley: file(
+          absolutePath: { regex: "/partners/alex-kelley.png/" }
+        ) {
+          ...fluidImage
+        }
+        nickDivehall: file(
+          absolutePath: { regex: "/partners/nick-divehall.png/" }
+        ) {
+          ...fluidImage
+        }
+        markLindon: file(
+          absolutePath: { regex: "/partners/mark-lindon.png/" }
+        ) {
+          ...fluidImage
+        }
+        lukePeterson: file(
+          absolutePath: { regex: "/partners/luke-peterson.png/" }
+        ) {
+          ...fluidImage
+        }
+        reubenTucker: file(
+          absolutePath: { regex: "/partners/reuben-tucker.png/" }
+        ) {
+          ...fluidImage
+        }
+      }
+    `}
+    render={data => {
+      const alexKelley = data.alexKelley.childImageSharp.fluid;
+      const nickDivehall = data.nickDivehall.childImageSharp.fluid;
+      const markLindon = data.markLindon.childImageSharp.fluid;
+      const lukePeterson = data.lukePeterson.childImageSharp.fluid;
+      const reubenTucker = data.reubenTucker.childImageSharp.fluid;
+      return (
+        <Container>
+          <SectionHeader>Meet Our Team</SectionHeader>
+          <Photos>
+            <Photo>
+              <Img fluid={nickDivehall} alt="Nick Divehall" />
+            </Photo>
+            <Photo>
+              <Img fluid={alexKelley} alt="Alex Kelley" />
+            </Photo>
+            <Photo>
+              <Img fluid={markLindon} alt="Mark Lindon" />
+            </Photo>
+            <Photo>
+              <Img fluid={lukePeterson} alt="Luke Peterson" />
+            </Photo>
+            <Photo>
+              <Img fluid={reubenTucker} alt="Reuben Tucker" />
+            </Photo>
+          </Photos>
+        </Container>
+      );
+    }}
+  />
 );
 
 export default TeamSection;
