45afebc36effb9773fd2e766141924a2bff13909
Alex Kelley <adkelley@mac.com>
2019-05-22 17:21:36 -0700

    add: form styling

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,18 +4,61 @@ import styled from "styled-components";
 import Layout from "../components/layout";
 import { SectionContainer, SectionHeading } from "../components/styled";
 
-const FormFieldStyle = styled.div`
-  margin: 2rem 5rem 2rem 5rem;
+const FormContainer = styled.div`
+  margin: 4vh 0 4vh 0;
 `;
 
-const labelStyle = {
-  color: "var(--secondary-color)",
-  fontSize: "var(--h2-font-size)",
-};
+const FormFieldStyle = styled.div`
+  margin: 2vh 5vw 2vh 5vw;
 
-const abbrStyle = {
-  color: "var(--ternary-color)",
-};
+  .textBox {
+    display: block;
+    margin-top: 1vh;
+    border: 2px solid transparent;
+    border-radius: 0.8rem 0;
+    padding: 1.5rem 1.6rem;
+    background: var(--secondary-color);
+    line-height: 1.4;
+    font-size: var(--p-font-size);
+    font-color: var(--primary-color);
+    transition: border-color 0.1s ease-out;
+    width: 45vw;
+
+    &:hover {
+      border-color: var(--primary-color);
+    }
+  }
+
+  .abbr {
+    color: var(--ternary-color);
+  }
+
+  .label {
+    color: var(--secondary-color);
+    font-size: var(--h2-font-size);
+  }
+
+  .button {
+    font-size: var(--p-font-size);
+    color: var(--secondary-color);
+    letter-spacing: var(--p-letter-spacing);
+    padding: 1.5rem 1.6rem;
+    border-radius: 0.8rem 0;
+    text-transform: uppercase;
+    border: 1px solid;
+    transform: translate3d(0, 0, 0);
+    transition: all 0.2s ease-out;
+    z-index: 1;
+    background: hsla(36, 6%, 85%, 0%);
+    user-select: none;
+
+    &:hover {
+      border-color: var(--primary-color);
+      color: var(--ternary-color);
+      background: hsla(36, 6%, 85%, 100%);
+    }
+  }
+`;
 
 function Form() {
   const [name, setName] = useState("");
@@ -30,119 +73,119 @@ function Form() {
     <Layout>
       <SectionContainer>
         <SectionHeading>
-          {"We\u2019d love to hear from you and see what you are building"}
+          {"We\u2019d love to hear from you and see what you are building."}
         </SectionHeading>
-        <form
-          name="contact"
-          method="post"
-          action="/success/"
-          data-netlify="true"
-          data-netlify-honeypot="botField"
-        >
-          <input name="form-name" type="hidden" value="contact" />
-          <p hidden>
-            <label>Do not fill this out:</label>
-            <input
-              name="botField"
-              onChange={e => setBotField(e.target.value)}
-              type="hidden"
-              value={botField}
-            />
-          </p>
-          <FormFieldStyle>
-            <label htmlFor="name" style={labelStyle}>
-              What is your name?
-              <abbr title="required" style={abbrStyle}>
-                {"\u0020*"}
-              </abbr>
-            </label>
-            <div>
+        <FormContainer>
+          <form
+            name="contact"
+            method="post"
+            action="/success/"
+            data-netlify="true"
+            data-netlify-honeypot="botField"
+          >
+            <input name="form-name" type="hidden" value="contact" />
+            <p hidden>
+              <label>Do not fill this out:</label>
+              <input
+                name="botField"
+                onChange={e => setBotField(e.target.value)}
+                type="hidden"
+                value={botField}
+              />
+            </p>
+            <FormFieldStyle>
+              <label htmlFor="name" className="label">
+                What is your name?
+                <span title="Required field" className="abbr">
+                  <span aria-hidden="true"> {"\u0020*"}</span>
+                </span>
+              </label>
               <input
                 value={name}
                 onChange={e => setName(e.target.value)}
                 placeholder="Jane Smith"
                 type="text"
                 name="name"
+                className="textBox"
                 required
               />
-            </div>
-          </FormFieldStyle>
-          <FormFieldStyle>
-            <label htmlFor="name" style={labelStyle}>
-              What is your email address?
-              <abbr title="required" style={abbrStyle}>
-                {"\u0020*"}
-              </abbr>
-            </label>
-            <div>
+            </FormFieldStyle>
+            <FormFieldStyle>
+              <label htmlFor="name" className="label">
+                What is your email address?
+                <span title="Required field" className="abbr">
+                  <span aria-hidden="true"> {"\u0020*"}</span>
+                </span>
+              </label>
               <input
                 value={email}
                 onChange={e => setEmail(e.target.value)}
                 placeholder="name@domain.com"
                 type="email"
                 name="email"
+                className="textBox"
                 required
               />
-            </div>
-          </FormFieldStyle>
-          <FormFieldStyle>
-            <label htmlFor="name" style={labelStyle}>
-              What is the name of your company?
-              <abbr title="required" style={abbrStyle}>
-                {"\u0020*"}
-              </abbr>
-            </label>
-            <div>
+            </FormFieldStyle>
+            <FormFieldStyle>
+              <label htmlFor="name" className="label">
+                What is the name of your company?
+                <span title="Required field" className="abbr">
+                  <span aria-hidden="true"> {"\u0020*"}</span>
+                </span>
+              </label>
               <input
                 value={companyName}
                 onChange={e => setCompanyName(e.target.value)}
                 placeholder="Company Name"
                 type="text"
                 name="companyName"
+                className="textBox"
                 required
               />
-            </div>
-          </FormFieldStyle>
-          <FormFieldStyle>
-            <label htmlFor="name" style={labelStyle}>
-              What stage is your company?
-              <abbr title="required" style={abbrStyle}>
-                {"\u0020*"}
-              </abbr>
-            </label>
-            <div>
+            </FormFieldStyle>
+            <FormFieldStyle>
+              <label htmlFor="name" className="label">
+                What stage is your company?
+                <span title="Required field" className="abbr">
+                  <span aria-hidden="true"> {"\u0020*"}</span>
+                </span>
+              </label>
               <input
                 value={stage}
                 onChange={e => setStage(e.target.value)}
                 placeholder="Seed, Early, or Growth"
                 type="text"
                 name="stage"
+                className="textBox"
                 required
               />
-            </div>
-          </FormFieldStyle>
-          <FormFieldStyle>
-            <label htmlFor="name" style={labelStyle}>
-              Please describe your company in one sentence
-              <abbr title="required" style={abbrStyle}>
-                {"\u0020*"}
-              </abbr>
-            </label>
-            <div>
+            </FormFieldStyle>
+            <FormFieldStyle>
+              <label htmlFor="name" className="label">
+                Please describe your company in one sentence.
+                <span title="Required field" className="abbr">
+                  <span aria-hidden="true"> {"\u0020*"}</span>
+                </span>
+              </label>
+              w{" "}
               <input
                 value={sentence}
                 onChange={e => setSentence(e.target.value)}
                 placeholder="We are building . . ."
                 type="text"
                 name="sentence"
+                className="textBox"
                 required
               />
-            </div>
-          </FormFieldStyle>
-          <FormFieldStyle>
-            <button type="submit">Submit</button>
-          </FormFieldStyle>
-        </form>
+            </FormFieldStyle>
+            <FormFieldStyle>
+              <button type="submit" className="button">
+                submit
+              </button>
+            </FormFieldStyle>
+          </form>
+        </FormContainer>
       </SectionContainer>
     </Layout>
   );
