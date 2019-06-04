d5343abab3179d8ccf27f9f0df3d1dba7e8a30e0
Alex Kelley <adkelley@mac.com>
2019-06-03 17:42:51 -0700

    mod: header wip

--- src/components/header.js
+++ src/components/header.js
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 import styled from "styled-components";
 import { graphql, StaticQuery, Link } from "gatsby";
 import BackgroundImage from "gatsby-background-image";
+import MenuItemStyle from "./styled";
 import { injectIntl } from "gatsby-plugin-intl";
 
 import { logo, logoHighlight } from "../resources/vector-graphics";
@@ -11,11 +12,24 @@ const HeaderContainer = styled.div`
   margin: 2vh 5vw 10vh 5vw;
 `;
 
+const MenuContainer = styled.div`
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: space-between;
+  align-items: center;
+
+  .logo {
+    /* Limit hover to size of Logo */
+    flex-basis: 182px;
+  }
+`;
+
 const Description = styled.div`
   padding-top: 0.5vh;
   color: var(--secondary-color);
   font-size: calc(var(--p-font-size) * 0.75);
   letter-spacing: calc(var(--p-letter-spacing) * 0.75);
+  text-transform: uppercase;
 `;
 
 const Tagline = styled.div`
@@ -23,11 +37,7 @@ const Tagline = styled.div`
   color: var(--secondary-color);
   font-size: var(--h2-font-size);
   letter-spacing: var(--h2-letter-spacing);
-`;
-
-const Logo = styled.div`
-  /* restrict hover to size of logo */
-  width: 182px;
+  text-transform: uppercase;
 `;
 
 const HeaderSection = ({ location, className, intl }) => {
@@ -57,16 +67,20 @@ const HeaderSection = ({ location, className, intl }) => {
             backgroundColor={`#040e18`}
           >
             <HeaderContainer>
-              <Logo
-                onMouseEnter={() => setHover(true)}
-                onMouseLeave={() => setHover(false)}
-              >
-                {notIndexRoute ? (
-                  <Link to="/">{hover ? logoHighlight : logo}</Link>
-                ) : (
-                  logo
-                )}
-              </Logo>
+              <MenuContainer>
+                <div
+                  className="logo"
+                  onMouseEnter={() => setHover(true)}
+                  onMouseLeave={() => setHover(false)}
+                >
+                  {notIndexRoute ? (
+                    <Link to="/">{hover ? logoHighlight : logo}</Link>
+                  ) : (
+                    logo
+                  )}
+                </div>
+                <MenuItemStyle>{`English`}</MenuItemStyle>
+              </MenuContainer>
               <Description>
                 {intl.formatMessage({ id: "headerDescription" })}
               </Description>
