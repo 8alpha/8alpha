0c1bf88d66c51999624b2ba6a3b10f083e36257e
Alex Kelley <adkelley@mac.com>
2019-06-14 17:14:47 -0700

    refact: useStaticQuery hook

--- src/pages/team.js
+++ src/pages/team.js
@@ -1,12 +1,12 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { graphql, StaticQuery } from "gatsby";
+import { graphql, useStaticQuery } from "gatsby";
 import Img from "gatsby-image";
 import styled from "styled-components";
 import { injectIntl } from "gatsby-plugin-intl";
 
 import { SectionStyle } from "../components/styled";
-import { linkedIn } from "../resources/icons";
+/* import { linkedIn } from "../resources/icons"; */
 import gatsbyIntlLanguage from "../utilities/gatsbyintllanguage";
 import Layout from "../components/layout";
 
@@ -36,93 +36,86 @@ const Image = styled(Img)`
   }
 `;
 
-const Team = ({ intl }) => (
-  <StaticQuery
-    query={graphql`
-      fragment fluidImage on File {
-        childImageSharp {
-          fluid(maxWidth: 480) {
-            ...GatsbyImageSharpFluid_withWebp
-          }
+const Team = ({ intl }) => {
+  const data = useStaticQuery(graphql`
+    fragment fluidImage on File {
+      childImageSharp {
+        fluid(maxWidth: 480) {
+          ...GatsbyImageSharpFluid_withWebp
         }
       }
-      query {
-        alexKelley: file(
-          absolutePath: { regex: "/partners/alex-kelley.png/" }
-        ) {
-          ...fluidImage
-        }
-        nickDivehall: file(
-          absolutePath: { regex: "/partners/nick-divehall.png/" }
-        ) {
-          ...fluidImage
-        }
-        markLindon: file(
-          absolutePath: { regex: "/partners/mark-lindon.png/" }
-        ) {
-          ...fluidImage
-        }
-        lukePeterson: file(
-          absolutePath: { regex: "/partners/luke-peterson.png/" }
-        ) {
-          ...fluidImage
-        }
-        reubenTucker: file(
-          absolutePath: { regex: "/partners/reuben-tucker.png/" }
-        ) {
-          ...fluidImage
-        }
+    }
+    query {
+      alexKelley: file(absolutePath: { regex: "/partners/alex-kelley.png/" }) {
+        ...fluidImage
       }
-    `}
-    render={data => {
-      const language = gatsbyIntlLanguage();
+      nickDivehall: file(
+        absolutePath: { regex: "/partners/nick-divehall.png/" }
+      ) {
+        ...fluidImage
+      }
+      markLindon: file(absolutePath: { regex: "/partners/mark-lindon.png/" }) {
+        ...fluidImage
+      }
+      lukePeterson: file(
+        absolutePath: { regex: "/partners/luke-peterson.png/" }
+      ) {
+        ...fluidImage
+      }
+      reubenTucker: file(
+        absolutePath: { regex: "/partners/reuben-tucker.png/" }
+      ) {
+        ...fluidImage
+      }
+    }
+  `);
 
-      const cards = [
-        {
-          photo: data.nickDivehall.childImageSharp.fluid,
-          alt: "Nick Divehall",
-          slug: "nickdivehall"
-        },
-        {
-          photo: data.alexKelley.childImageSharp.fluid,
-          alt: "Alex Kelley",
-          slug: "alexdkelley"
-        },
-        {
-          photo: data.markLindon.childImageSharp.fluid,
-          alt: "Mark Lindon",
-          slug: "mark-lindon-0656a1104"
-        },
-        {
-          photo: data.lukePeterson.childImageSharp.fluid,
-          alt: "Luke Peterson",
-          slug: "lupeterson"
-        },
-        {
-          photo: data.reubenTucker.childImageSharp.fluid,
-          alt: "Reuben Tucker",
-          slug: "reuben-tucker-5212b269"
-        }
-      ];
+  const language = gatsbyIntlLanguage();
 
-      const Photos = () =>
-        cards.map(card => (
-          <Image key={card.alt} fluid={card.photo} alt={card.alt} />
-        ));
+  const cards = [
+    {
+      photo: data.nickDivehall.childImageSharp.fluid,
+      alt: "Nick Divehall",
+      slug: "nickdivehall"
+    },
+    {
+      photo: data.alexKelley.childImageSharp.fluid,
+      alt: "Alex Kelley",
+      slug: "alexdkelley"
+    },
+    {
+      photo: data.markLindon.childImageSharp.fluid,
+      alt: "Mark Lindon",
+      slug: "mark-lindon-0656a1104"
+    },
+    {
+      photo: data.lukePeterson.childImageSharp.fluid,
+      alt: "Luke Peterson",
+      slug: "lupeterson"
+    },
+    {
+      photo: data.reubenTucker.childImageSharp.fluid,
+      alt: "Reuben Tucker",
+      slug: "reuben-tucker-5212b269"
+    }
+  ];
 
-      return (
-        <Layout location="/team/">
-          <SectionStyle>
-            <h2 lang={language}>{intl.formatMessage({ id: "teamHeading" })}</h2>
-            <PhotosContainer>
-              <Photos />
-            </PhotosContainer>
-          </SectionStyle>
-        </Layout>
-      );
-    }}
-  />
-);
+  const Photos = () =>
+    cards.map(card => (
+      <Image key={card.alt} fluid={card.photo} alt={card.alt} />
+    ));
+
+  return (
+    <Layout location="/team/">
+      <SectionStyle>
+        <h2 lang={language}>{intl.formatMessage({ id: "teamHeading" })}</h2>
+        <PhotosContainer>
+          <Photos />
+        </PhotosContainer>
+      </SectionStyle>
+    </Layout>
+  );
+};
 
 Team.propTypes = {
   intl: PropTypes.object.isRequired
