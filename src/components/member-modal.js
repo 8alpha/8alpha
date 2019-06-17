7ede97d3c9428385283930d9db6767f3ff0c7995
Alex Kelley <adkelley@mac.com>
2019-06-16 17:52:32 -0700

    mod: modal w/ display grid

--- src/components/member-modal.js
+++ src/components/member-modal.js
@@ -13,28 +13,49 @@ const CloseButton = styled.button`
   background-color: rgba(0, 0, 0, 0);
   text-decoration: none;
   cursor: pointer;
+  float: right;
 `;
 
 const Modal = styled(ReactModal)`
   margin: 20rem auto;
   width: 70%;
   border: 2px solid var(--primary-color);
-  padding: 10px;
-
-  h2 {
-    font-size: var(--h2-font-size);
-    color: var(--primary-color);
-  }
-
-  p {
-    font-size: var(--p-font-size);
-    color: var(--secondary-color);
-  }
+  padding: 1.5rem;
 `;
 
+const ContentBox = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 3fr;
+  grid-gap: 1.5rem;
+`;
+
+const PhotoBox = styled.div``;
+
 const Image = styled(Img)`
-  width: 25%;
-  float: left;
+  margin-bottom: 1rem;
+  grid-area: photo;
+`;
+
+const LinkedIn = styled.a`
+  text-decoration: none;
+  float: right;
+`;
+
+const TextBox = styled.div``;
+
+const Name = styled.div`
+  font-size: var(--h2-font-size);
+  color: var(--primary-color);
+`;
+
+const Title = styled.div`
+  font-size: var(--h2-font-size);
+  color: var(--primary-color);
+`;
+
+const Bio = styled.div`
+  font-size: var(--p-font-size);
+  color: var(--secondary-color);
 `;
 
 ReactModal.setAppElement(`#___gatsby`);
@@ -60,14 +81,22 @@ const MemberModal = ({ member, intl }) => {
         },
       }}
     >
-      <Link to="/team/">
-        <CloseButton onClick={handleCloseModal}>{close}</CloseButton>
-      </Link>
-      <Image fluid={member.image.childImageSharp.fluid} alt={member.name} />
-      <a href={`https://www.linkedin.com/in/${member.linkedIn}`}>{linkedIn}</a>
-      <h2>{intl.formatMessage({ id: member.name })}</h2>
-      <h2>{intl.formatMessage({ id: member.jobTitle })}</h2>
-      <div dangerouslySetInnerHTML={biographyHTML()} />
+      <ContentBox>
+        <PhotoBox>
+          <Image fluid={member.image.childImageSharp.fluid} alt={member.name} />
+          <LinkedIn href={`https://www.linkedin.com/in/${member.linkedIn}`}>
+            {linkedIn}
+          </LinkedIn>
+        </PhotoBox>
+        <TextBox>
+          <Link to="/team/">
+            <CloseButton onClick={handleCloseModal}>{close}</CloseButton>
+          </Link>
+          <Name>{intl.formatMessage({ id: member.name })}</Name>
+          <Title>{intl.formatMessage({ id: member.jobTitle })}</Title>
+          <Bio dangerouslySetInnerHTML={biographyHTML()} />
+        </TextBox>
+      </ContentBox>
     </Modal>
   );
 };
