c5f8c30c865a56cc903a692ef13510de82037e96
Alex Kelley <adkelley@mac.com>
2019-07-15 16:28:52 -0700

    wip: select text, add: hover, select

--- src/components/technologies.js
+++ src/components/technologies.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 import { injectIntl } from "gatsby-plugin-intl";
@@ -7,14 +7,14 @@ import { Section } from "../css/style";
 
 import {
   TechFinanceTile,
-  TechAIMLTile,
-  TechCGTile,
+  TechAiTile,
+  TechImagingTile,
   TechEnterpriseTile,
 } from "../resources/techTiles";
 
 const TechBox = styled.div`
-  margin-left: 20rem;
-  margin-right: 20rem;
+  margin-left: 30rem;
+  margin-right: 30rem;
   display: grid;
 
   @media screen and (max-width: 599px) {
@@ -32,21 +32,115 @@ const TechBox = styled.div`
   @media screen and (min-width: 1200px) {
     grid-template-columns: 1fr 1fr;
     grid-template-rows: auto;
-    grid-gap: 4vh 4vw;
+    grid-gap: 2vh 2vw;
   }
 `;
 
-const Technologies = ({ intl }) => (
-  <Section>
-    <h2 lang={intl.locale}>{intl.formatMessage({ id: "techHeading" })}</h2>
-    <TechBox>
-      <TechFinanceTile intl={intl} />
-      <TechAIMLTile intl={intl} />
-      <TechCGTile intl={intl} />
-      <TechEnterpriseTile intl={intl} />
-    </TechBox>
-  </Section>
-);
+const Tile = styled.div`
+  a {
+    text-decoration: none;
+  }
+`;
+
+const Technologies = ({ intl }) => {
+  const defaultTileSelectStates = {
+    finance: false,
+    ai: false,
+    imaging: false,
+    enterprise: false,
+  };
+
+  const defaultTileHoverStates = {
+    finance: false,
+    ai: false,
+    imaging: false,
+    enterprise: false,
+  };
+
+  const [tileSelectStates, setSelectState] = useState(defaultTileSelectStates);
+  const [tileHoverStates, setHoverState] = useState(defaultTileHoverStates);
+
+  const handleTileSelect = (e, tileName) => {
+    e.preventDefault();
+
+    Object.entries(tileSelectStates).map(([key]) => {
+      if (key === tileName) {
+        tileSelectStates[key] = true;
+      } else {
+        tileSelectStates[key] = false;
+      }
+    });
+
+    setSelectState({ ...defaultTileSelectStates, ...tileSelectStates });
+  };
+
+  const handleHover = (hoverState, tile) => {
+    Object.entries(tileHoverStates).map(([key]) => {
+      if (key === tile) {
+        tileHoverStates[key] = hoverState;
+      }
+    });
+
+    setHoverState({ ...defaultTileHoverStates, ...tileHoverStates });
+  };
+
+  const renderTile = (tile, tileFn) => (
+    <Tile
+      onMouseEnter={() => handleHover(true, tile)}
+      onMouseLeave={() => handleHover(false, tile)}
+    >
+      <a href="#" onClick={e => handleTileSelect(e, tile)}>
+        {tileFn}
+      </a>
+    </Tile>
+  );
+
+  return (
+    <Section>
+      <h2 lang={intl.locale}>{intl.formatMessage({ id: "techHeading" })}</h2>
+      {tileSelectStates.finance && <p>Finance Tile Selected</p>}
+      {tileSelectStates.ai && <p>AI Machine Learning Tile Selected</p>}
+      {tileSelectStates.imaging && <p>CG & Image Processing Tile Selected</p>}
+      {tileSelectStates.enterprise && <p>Enterprise Tile Selected</p>}
+      <TechBox>
+        {renderTile(
+          "finance",
+          <TechFinanceTile
+            intl={intl}
+            hoverState={tileHoverStates.finance}
+            selectState={tileSelectStates.finance}
+          />
+        )}
+        {renderTile(
+          "ai",
+          <TechAiTile
+            intl={intl}
+            hoverState={tileHoverStates.ai}
+            selectState={tileSelectStates.ai}
+          />
+        )}
+
+        {renderTile(
+          "imaging",
+          <TechImagingTile
+            intl={intl}
+            hoverState={tileHoverStates.imaging}
+            selectState={tileSelectStates.imaging}
+          />
+        )}
+
+        {renderTile(
+          "enterprise",
+          <TechEnterpriseTile
+            intl={intl}
+            hoverState={tileHoverStates.enterprise}
+            selectState={tileSelectStates.enterprise}
+          />
+        )}
+      </TechBox>
+    </Section>
+  );
+};
 
 Technologies.propTypes = {
   intl: PropTypes.object.isRequired,
---
+++ src/resources/theme.js
@@ -0,0 +1,6 @@
+export const themeColors = {
+  background: "#001824",
+  primary: "#8b8b8d",
+  secondary: "#d6d6d6",
+  ternary: "#c32c4b",
+};
